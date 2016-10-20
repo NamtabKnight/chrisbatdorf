8704232f38546119a3692bcabe6719e965c3e5a4
NamtabKnight <cjbatdorf@gmail.com>
2016-10-19 23:10:05 -0400

    initial commit after the start of freelance website

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Chris Batdorf - Web Development</title>
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+
+        <!-- custom css -->
+        <link href="css/main.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class="container-fluid" id="header">
+            <h1><img src="assets/logo/chrisbatdorf_logo_300.png" width="300" height="300" alt="Chris Batdorf Web Development Logo" /></h1>
+        </div>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!--<a class="navbar-brand" href="#">Brand</a>-->
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+                        <li><a href="#">Why Me?</a></li>
+                        <li><a href="#">Who Am I?</a></li>
+                        <li><a href="#">Where Am I?</a></li>
+                        <li><a href="#">How long?</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>  
+        <div class="container-fluid">
+            <div class="col-lg-1"></div>
+            <div class="col-lg-10" id="main_content">
+                <h1>Web Development!</h1>
+                <p>Hello There!</p>
+                <p>I provide full Web Development services. I work with each client to tailor a solution for each client that not only works for them now but any future plans as well. I have years of experience creating both hand coded, W3C compliant websites as well as utilizing popular frameworks such as WordPress, LightCMS, and Joomla to create both informational websites as well as eCommerce solutions.
+After the initial development is complete, I offer monthly maintenance plans to keep your solution up-to-date.</p>
+
+            </div>
+        </div>
+        
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
